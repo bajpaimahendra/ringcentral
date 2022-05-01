1a77c2e030b2f55cd14f4df59b25f466ba260da6
Mahendra <bajpai.mahendra@gmail.com>
2022-05-01 20:47:17 +0530

    okay

--- _credentials-Sample.php
+++ _credentials-Sample.php
@@ -4,8 +4,8 @@ return array(
     'username'     => '16165486841', // your RingCentral account phone number
     'extension'    =>  null, // or number
     'password'     => '',
-    'clientId'     => 'j8R7CXLTQE-gTd8zQnmhqw',
-    'clientSecret' => 'I4aiTIEyQNqfHkNcVSqPuQydJ-GVuuR3Kt94ZhfAKogw',
+    'clientId'     => '',
+    'clientSecret' => '',
     'server'       => 'https://platform.devtest.ringcentral.com', // for Sandbox - 
     //'server'       => 'https://platform.ringcentral.com', // for production - 
     'smsNumber'    => '18882223344', // any of SMS-enabled numbers on your RingCentral account
